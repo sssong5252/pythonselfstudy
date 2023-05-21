9ffc891fdc8b2e3674e48bad202bcccdc5cad2fa
sssong5252 <luckyjude@naver.com>
2023-05-21 17:28:47 +0900

    secwork

--- .vscode/hello.py
+++ .vscode/hello.py
@@ -1,43 +1,63 @@
-import turtle as t
-
-t.speed(0)
-t.hideturtle()
-
-# x1,y1 - x2,y2 직선 그리기
-def line(x1,y1,x2,y2):
-    t.up()
-    t.goto(x1,y1)
-    t.down()
-    t.goto(x2,y2)
-    return
-
-# x,y에 텍스트 쓰기
-def write(x,y,text):
-    t.up()
-    t.goto(x,y)
-    t.down()
-    t.write(text)
-    return
-
-# 좌표평면
-line(-500,0,500,0)
-line(0,-500,0,500)
-
-# x축 눈금 그리기
-i=-500
-while i<=500:
-    i=i+100
-    line(i,-5,i,5)
-    if i != 0:
-        write(i-10,-20,i)
-
-
-# y축 눈금 그리기
-i = -500
-while i <= 500:
-    i = i + 100
-    line(-5, i, 5, i)
-    if i!=0:
-        write(7, i-5, i)
-
-t.done()
+# number = 10
+# day = "three"
+# print("i ate %d apples. so i was sick for %s days" %(number, day))
+# print("i have %s apples" %3)
+# print("rate is %s" %3.234)
+# print("%10s" %"hi")
+# print("%-10sjane" %'hi')
+# print("%0.4f" %3.45345565)
+# print("%10.4f" %3.42098750)
+# print("i eat {0} apples".format(3))
+# print("i eat {0} apples".format("five"))
+# print("i eat {0} apples".format(number))
+# print("i ate {0} apples. so i was sick for {1} days".format(number, day))
+# print("i ate {0} apples. so i was sick for {day} days".format(10, day = 3))
+# print("{0:10}".format("hi"))
+# a = "life is too short"
+# print(a.replace("life", "your leg"))
+# a = [1, 2, 3]
+# print(a)
+# print(a[0])
+# a = [1, 2, 3]
+# a[2] = 4
+# print(a)
+# a = [1, 2, 3]
+# del a[1]
+# # print(a)
+# t1 = (1, 2,'a', 'b')
+# t2 = (3, 4)
+# print(t1 * t2)
+# a = {1: 'a'}
+# a[2] = 'b'
+# print(a)
+# a['name'] = 'pey'
+# a[3] = [1, 2, 3]
+# del a[1]
+# print(a)
+# grade = {'pey' : 10, 'juliet' : 99}
+# print(grade['pey'])
+# print(grade['juliet'])
+# a = {1 : 'a', 2 : 'b'}
+# print(a[1])
+# print(a[2])
+# a = {'a' : 1, 'b' : 2}
+# print(a['a'])
+# print(a['b'])
+# dic = {'name' : 'pey', 'phone' : '0119993323', 'birth' : '1118'}
+# print(dic['name'])
+# print(dic['phone'])
+# print(dic['birth'])
+# a = {'a' : 1, 'b' : 2}
+# print(a['a'])
+# print(a['b'])
+# a = {'name' : 'pey' , 'phone' : '011'}
+a = {'name' : 'pey', 'phone' : '0119993323', 'birth' : '1118'}
+for k in a.keys():
+    print(k)
+print(list(a.keys()))
+print(a.values())
+print(a.items())
+# print(a.clear())
+# print(a)
+print(a.get('name'))
+print(a.get('phone'))
