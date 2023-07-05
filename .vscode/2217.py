c85e03c6115752e271b17492577606fabf9963a2
sssong5252 <luckyjude@naver.com>
2023-07-05 15:43:28 +0900

    study

---
+++ .vscode/2217.py
@@ -0,0 +1,12 @@
+c = int(input())
+m = []
+for i in range(n):
+    m.append(int(input()))
+    
+m.sort(reverse = True)
+result = []
+
+for j in range(n):
+    result.append(m[j]*(j + 1))
+    
+print(max(result))
